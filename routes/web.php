16f1e37ce629df34c71b60f5a760169010afe034
Dawid Skibński <skibol2@wp.pl>
2023-09-07 13:06:46 +0200

    dodanie routing dla generate-barcode

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\BarcodeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::match(['get', 'post'], '/generate-barcode', [BarcodeController::class, 'generate'])->name('generate.barcode');
